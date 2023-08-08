6fa0462b710c662593339d9a1568bdd87ef0418d
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-08-08 15:01:58 +0200

    Update btc.py

--- btc.py
+++ btc.py
@@ -21,15 +21,16 @@ while True:
 
     time.sleep(300) # wait 5 minutes
 
-'''
+
 # get multi Cryptocurrency price from Binance Platform as windows notificatin
+
 import requests
 from win10toast import ToastNotifier
 import time
 from binance.client import Client
 
 toaster = ToastNotifier()
-client = Client ('3Pc9cskMw0L54wrHdUSj5v1pXB5VY5sZwEcUOVV9SoDbx811lyO2nOYYGD11NdaQ' , 'I9zyRmVAHm47peOmlKdkjXvFw5KZbAgLMhCQwGyKzQ3f8mtsrHgmxKFXAEKMp9UZ ')
+client = Client ('<API Key>' , '<Secret Key>')
 
 binance_symbols = ['BTCUSDT',  'VETUSDT']
 while True:
@@ -38,7 +39,7 @@ while True:
         price = float([p['price'] for p in prices if p['symbol'] == symbol][0])
         toaster.show_toast(f"{symbol} Price", f"${price:,.2f}", duration=10)
     time.sleep(300) # wait 5 minutes
-'''
+
 
 
 
